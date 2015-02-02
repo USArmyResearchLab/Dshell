d775242fb23187754d69574a2b44cb6094c0e413
unknown <matr06586@D06586.core.internal.corp>
2015-02-02 10:42:32 -0600

    Attempt to keep line length legible and below 80 characters with small exceptions.

--- lib/dnsdecoder.py
+++ lib/dnsdecoder.py
@@ -3,20 +3,20 @@
 import dshell, util, dpkt
 
 class DNSDecoder(dshell.TCPDecoder):
-    '''extend DNSDecoder to handle DNS request/responses
-            pairs request and response(s) by connection and query ID
-            to allow for detection of DNS spoofing, etc.. (multiple responses to request with same ID)
-            will call DNSHandler(
-                                                            conn=Connection(),
-                                                            request=dpkt.dns.DNS,
-                                                            response=dpkt.dns.DNS,
-                                                            requesttime=timestamp, responsetime=timestamp,
-                                                            responsecount=responsecount
-                                                    )
-            after each response.
+    '''
+    Extend DNSDecoder to handle DNS request/responses. Pairs request and 
+    response(s) by connection and query ID to allow for detection of DNS 
+    spoofing, etc.. (multiple responses to request with same ID will call
 
-            config: noanswer: if True and discarding w/o response, will call with response,responsetime=None,None (True)
+    DNSHandler(conn=Connection(), request=dpkt.dns.DNS,
+               response=dpkt.dns.DNS, requesttime=timestamp, 
+               responsetime=timestamp, responsecount=responsecount
+               )
 
+    after each response.
+
+    config: noanswer: if True and discarding w/o response, will call with 
+    response,responsetime=None, None (True)
     '''
     def __init__(self,**kwargs):
         self.noanswer=True
@@ -25,10 +25,14 @@ class DNSDecoder(dshell.TCPDecoder):
         self.maxblobs=None
 
     def blobHandler(self,conn,blob):
-        '''for each blob, examine each segment (UDP packet) seperately as each will be a DNS Q/A
-                pair Q/A by ID and return as pairs'''
+        '''
+        For each blob, examine each segment (UDP packet) separately as each 
+        will be a DNS Q/A pair Q/A by ID and return as pairs
+        '''
         connrqs=self.requests.setdefault(conn,{})
-        for data in blob: #iterate blob as each packet will be a seperate request (catches spoofing)
+        # Iterate blob as each packet will be a separate request. 
+        # (catches spoofing)
+        for data in blob: 
             try: dns=dpkt.dns.DNS(data)
             except Exception,e: self._exc(e)
             if dns.qr == dpkt.dns.DNS_Q:
@@ -37,21 +41,28 @@ class DNSDecoder(dshell.TCPDecoder):
                 rq=connrqs.get(dns.id,[None,None,0])
                 rq[2]+=1
                 if "DNSHandler" in dir(self):
-                    self.DNSHandler(conn=conn,request=rq[1],response=dns,requesttime=rq[0],responsetime=blob.starttime,responsecount=rq[2])
+                    self.DNSHandler(conn=conn, request=rq[1], response=dns, 
+                                    requesttime=rq[0], 
+                                    responsetime=blob.starttime, 
+                                    responsecount=rq[2])
 
-    def connectionHandler(self,conn):
-        '''clean up unanswered requests when we discard the connection'''
-        if self.noanswer and "DNSHandler" in dir(self) and self.requests.get(conn):
-            for requesttime,request,responsecount in self.requests[conn].values():
+    def connectionHandler(self, conn):
+        '''Clean up unanswered requests when we discard the connection'''
+        requests = self.requests
+        if self.noanswer and "DNSHandler" in dir(self) and requests.get(conn):
+            for requesttime, request, responsecount in requests[conn].values():
                 if not responsecount:
-                    self.DNSHandler(conn=conn,request=request,response=None,requesttime=requesttime,responsetime=None,responsecount=responsecount)
+                    self.DNSHandler(conn=conn, request=request, response=None,
+                                    requesttime=requesttime, 
+                                    responsetime=None, 
+                                    responsecount=responsecount)
         if conn in self.requests: del self.requests[conn]
 
 class displaystub(dshell.Decoder):
     def __init__(self):
         dshell.Decoder.__init__(self,
                 name='dnsdecoder',
-                description='Intermediate class to support DNS based decoders.',
+                description='Intermediate class supporting DNS based decoders.',
                 longdescription="See source code or pydoc for details on use."
         )
 
