be7fb16ee86a2b780909f1a4f274697c5d8fbe40
Ava Gailliot <mztriz@gmail.com>
2015-02-03 12:56:56 -0500

    Fixed legacy BASH syntax
    Added double quotes to prevent dir globbing
    Changed legacy `...` syntax to $(...)

--- doc/generate-doc.sh
+++ doc/generate-doc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-d=`pwd`
+d=$(pwd)
 if [ "$1" ]; then d=$1; fi
-source $d/.dshellrc || exit
+source "$d"/.dshellrc || exit
 
-for f in $d/lib/*.py $d/lib/output/*.py $d/bin/*.py; do 
-        pydoc -w `basename $f|cut -d. -f1`
+for f in "$d"/lib/*.py $d/lib/output/*.py $d/bin/*.py; do 
+        pydoc -w $(basename "$f"|cut -d. -f1)
 done
 
-for f in `find $d/decoders -name \*.py -not -name __init__.py`; do
-	pydoc -w $f 
+for f in $(find "$d"/decoders -name \*.py -not -name __init__.py); do
+	pydoc -w "$f"
 done
