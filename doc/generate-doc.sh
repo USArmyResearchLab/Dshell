36b3d8066459138cc65cc845fbe9c06d7afea3cd
Ava Gailliot <mztriz@gmail.com>
2015-02-06 14:44:57 -0500

    missed double quote on $d var -line 6

--- doc/generate-doc.sh
+++ doc/generate-doc.sh
@@ -3,7 +3,7 @@ d=$(pwd)
 if [ "$1" ]; then d=$1; fi
 source "$d"/.dshellrc || exit
 
-for f in "$d"/lib/*.py $d/lib/output/*.py $d/bin/*.py; do 
+for f in "$d"/lib/*.py $d/lib/output/*.py "$d"/bin/*.py; do 
         pydoc -w $(basename "$f"|cut -d. -f1)
 done
 
