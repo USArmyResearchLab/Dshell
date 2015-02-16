301ec2ea6a188282f3c5013792268c407b588c83
raul5660 <rmartinez44@islander.tamucc.edu>
2015-02-15 22:54:23 -0600

    added the ability to automatically download geoIP files and check if certain files already exist just in case if its unnecessary to run make again

--- install-ubuntu.py
+++ install-ubuntu.py
@@ -2,6 +2,9 @@
 
 from pkgutil import iter_modules
 from subprocess import call
+import urllib
+import os
+import gzip
 
 dependencies = {
     "Crypto": "crypto",
@@ -25,4 +28,48 @@ if missing_pkgs:
     print(" ".join(cmd))
     call(cmd)
 
-call(["make", "all"])
+#get current path
+path = os.path.dirname(os.path.realpath(__file__))
+
+dshellrc = os.path.isfile(path+'/.dshellrc')
+dshell = os.path.isfile(path+'/dshell')
+dshell_decode = os.path.isfile(path+'/dshell-decode')
+
+#checks if files exist if not continues
+if not dshell and not dshellrc and  not dshell_decode:
+    call(["make", "all"])
+
+
+#used to download GeoIP Files
+geoFiles = [['GeoIP.dat','http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz'],
+            ['GeoIPv6.dat','http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz'],
+            ['GeoIPASNum.dat','http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz'],
+            ['GeoIPASNumv6.dat','http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz']]
+
+#append to directory
+path += '/share/GeoIP/'
+
+
+for geoFile,link in geoFiles:
+    #if file does not exist
+    if not os.path.isfile(path+geoFile+'.gz'):
+        try:
+            #download file
+            urllib.urlretrieve(link,path+geoFile+'.gz')
+            #open compressed file
+            inF = gzip.GzipFile(path+geoFile+'.gz','rb')
+            #read in compressed file
+            s = inF.read()
+            #close file
+            inF.close()
+
+            #open output file
+            outF = file(path+geoFile, 'wb')
+            #write output file
+            outF.write(s)
+            #close output file
+            outF.close()
+            #remove compressed file (just a little clean up!!)
+            os.remove(path+geoFile+'.gz')
+        except:
+            pass
