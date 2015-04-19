90eb0c5ada0c0b7b3aca6b3633cfc736e8150d87
Brehret N <initbar@users.noreply.github.com>
2015-04-18 23:46:44 -0400

    code styling and changed if logic.

--- doc/generate-doc.sh
+++ doc/generate-doc.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-d=`pwd`
-if [ "$1" ]; then d=$1; fi
+
+d=$(pwd)
+[ "$1" ] && d="$1"
 source $d/.dshellrc || exit
 
 for f in $d/lib/*.py $d/lib/output/*.py $d/bin/*.py; do 
-        pydoc -w `basename $f|cut -d. -f1`
+    pydoc -w $(basename $f | cut -d. -f1)
 done
 
-for f in `find $d/decoders -name \*.py -not -name __init__.py`; do
-	pydoc -w $f 
+for f in $(find $d/decoders -name \*.py -not -name __init__.py); do
+    pydoc -w $f 
 done
