0353ed78e6454f9297cbb022707a2c82a6d29d68
Michael R. Lawrence <mike@michaellawrenceit.com>
2015-11-16 22:01:38 -0600

    Update install-gentoo.py

--- install-gentoo.py
+++ install-gentoo.py
@@ -4,10 +4,16 @@ from pkgutil import iter_modules
 from subprocess import call
 
 dependencies = {
-    "Crypto": "crypto",
-    "dpkt": "dpkt",
-    "IPy": "ipy",
-    "pcap": "pypcap"
+    "Crypto": "dev-python/pycrypto",
+    "dpkt": "dev-python/dpkt",
+    "IPy": "dev-python/ipy",
+    "pcap": "dev-python/pypcap"
+    "pygeoip" : "dev-python/pygeoip"
+ 
+ 
+
+  
+ doc? (dev-python/epydoc)"
 }
 
 installed, missing_pkgs = [pkg[1] for pkg in iter_modules()], []
@@ -20,9 +26,18 @@ for module, pkg in dependencies.items():
         print("{} is installed".format(module))
 
 if missing_pkgs:
-    cmd = ["emerge", "apt-get", "install"] + missing_pkgs
+    cmd = ["emerge-sync emerge -v",] + missing_pkgs  # -av
 
     print(" ".join(cmd))
     call(cmd)
 
-call(["make", "all"])
+call(["make", "rc"]) ## Gentoo way is hell NO! , make all but docs, 
+call(["make", "initpy"]) ## Gentoo way is hell NO! , make all but docs, 
+ = input('Would You Like DSHELL DOCS?')
+if Docs == 'yes' or 'Yes':
+    dependencies = {
+        "pydoc":"dev-python/epydoc"
+        }
+ call(["make", "pydoc"])
+else:
+  print ("Sorry for asking...")
