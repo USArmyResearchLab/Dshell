a7c27c8a8099cabc9167d123bdfce4532cfc3c68
J <dev195@users.noreply.github.com>
2023-03-06 10:26:04 -0500

    Update setup.py to require pcapy-ng
    Changed setup.py install_requires list to replace the now unsupported pcapy library with pcapy-ng. Also increased Dshell version number and required Python version.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="Dshell",
-    version="3.2.1",
+    version="3.2.2",
     author="USArmyResearchLab",
     description="An extensible network forensic analysis framework",
     url="https://github.com/USArmyResearchLab/Dshell",
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     packages=find_packages(),
     package_data={
         "dshell": ["data/dshellrc", "data/GeoIP/readme.txt"],
@@ -20,7 +20,7 @@ setup(
     ],
     install_requires=[
         "geoip2",
-        "pcapy",
+        "pcapy-ng",
         "pypacker",
         "pyopenssl",
         "elasticsearch",
